e1f598c068d2e9147423d380447addf5310c43b5
Dalmiro <dalmiro.granias@gmail.com>
2015-03-29 18:39:21 -0300

    Video - Reference Types

---
+++ CS5Fundamentals/Grades/Grades/Gradebook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    class Gradebook
+    {
+        public Gradebook()
+        {
+            
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
---
+++ CS5Fundamentals/Grades/Grades/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gradebook book = new Gradebook();
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+
+
+         }
+    }
+}
