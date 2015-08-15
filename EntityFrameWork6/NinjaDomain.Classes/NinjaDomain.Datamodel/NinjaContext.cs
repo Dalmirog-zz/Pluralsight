6d819927a40a7fb88ac408a16457ab15a301e318
Dalmiro <dalmiro.granias@gmail.com>
2015-08-14 23:50:08 -0300

    End of Fixing how EF interprets you

---
+++ EntityFrameWork6/NinjaDomain.Classes/NinjaDomain.Datamodel/NinjaContext.cs
@@ -0,0 +1,12 @@
+﻿using NinjaDomain.Classes;
+using System.Data.Entity;
+
+namespace NinjaDomain.Datamodel
+{
+    public class NinjaContext:DbContext
+    {
+        public DbSet<NinjaDomain.Classes.Ninja> Ninjas { get; set; }
+        public DbSet<NinjaDomain.Classes.Clan> Clans { get; set; }
+        public DbSet<NinjaDomain.Classes.NinjaEquipment> Equipment { get; set; }
+    }
+}
