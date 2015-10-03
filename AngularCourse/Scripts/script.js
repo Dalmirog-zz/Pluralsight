5c3266ebe925a0812f000b55f74dbd1cef2a6c1d
Dalmiro <dalmiro.granias@gmail.com>
2015-10-03 18:20:28 -0300

    end of "Modules"

--- AngularCourse/Scripts/script.js
+++ AngularCourse/Scripts/script.js
@@ -1,24 +1,31 @@
 /**
  * Created by dalmi on 10/3/2015.
  */
-var work = function(){
 
-    console.log("Working hard!");
+var createWorker = function(){
 
+    var workCount = 0;
+
+    var task1 = function(){
+        workCount +=1;
+        console.log("task1 " + workCount);
+    };
+
+    var task2 = function(){
+        workCount +=1
+        console.log("task2 "+ workCount);
+    };
+
+    return{
+      job1: task1,
+      job2: task2
+    };
 };
 
-var doWork = function(f){
+var worker = createWorker();
 
-    console.log("Starting");
-
-    try{
-        f();
-    }
-    catch(ex){
-        console.log(ex);
-    }
-
-    console.log("Ending");
-};
-
-doWork(work);
+worker.job1();
+worker.job2();
+worker.job2();
+worker.job2();
+worker.job2();
