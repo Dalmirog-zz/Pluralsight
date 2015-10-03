bf74d9e3d882e45f0ea811512fcb91f507308e59
Dalmiro <dalmiro.granias@gmail.com>
2015-10-03 18:52:08 -0300

    end of "The IIFE"

--- AngularCourse/Scripts/script.js
+++ AngularCourse/Scripts/script.js
@@ -1,31 +1,34 @@
 /**
  * Created by dalmi on 10/3/2015.
  */
+//This is an IIFE (immediatelly invoked function expression)
+(function(){
 
-var createWorker = function(){
+    var createWorker = function(){
 
-    var workCount = 0;
+        var workCount = 0;
 
-    var task1 = function(){
-        workCount +=1;
-        console.log("task1 " + workCount);
+        var task1 = function(){
+            workCount +=1;
+            console.log("task1 " + workCount);
+        };
+
+        var task2 = function(){
+            workCount +=1
+            console.log("task2 "+ workCount);
+        };
+
+        return{
+            job1: task1,
+            job2: task2
+        };
     };
 
-    var task2 = function(){
-        workCount +=1
-        console.log("task2 "+ workCount);
-    };
+    var worker = createWorker();
 
-    return{
-      job1: task1,
-      job2: task2
-    };
-};
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job2();
+}());
 
-var worker = createWorker();
-
-worker.job1();
-worker.job2();
-worker.job2();
-worker.job2();
-worker.job2();
