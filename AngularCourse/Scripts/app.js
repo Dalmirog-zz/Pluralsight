fc73d4e72b246fbd5e8af53a1602a3262c864c1e
Dalmiro <dalmiro.granias@gmail.com>
2015-10-04 21:12:36 -0300

    end of course

--- AngularCourse/Scripts/app.js
+++ AngularCourse/Scripts/app.js
@@ -7,6 +7,10 @@
                 templateUrl: "main.html",
                 controller: "MainController"
             })
+            .when("/user/:username", {
+                templateUrl:"user.html",
+                controller: "UserController"
+            })
             .otherwise({redirectTo:"/main"});
     });
 }());
