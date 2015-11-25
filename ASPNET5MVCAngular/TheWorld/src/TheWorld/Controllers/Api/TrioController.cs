5187bb696dedcc9543b979a774af7dd10f94e54e
Dalmiro <dalmiro.granias@gmail.com>
2015-11-25 01:11:54 -0300

    end of "Returning Data"

--- ASPNET5MVCAngular/TheWorld/src/TheWorld/Controllers/Api/TrioController.cs
+++ ASPNET5MVCAngular/TheWorld/src/TheWorld/Controllers/Api/TrioController.cs
@@ -3,15 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Models;
 
 namespace TheWorld.Controllers.Api
 {
     public class TrioController : Controller
     {
+        private IWorldRepository _repository;
+
+        public TrioController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
         [HttpGet("api/trips")]
         public JsonResult Get()
         {
-            return Json(new { name = "Dalmiro" });
+            var results = _repository.GetAllTripsWithStops();
+            return Json(results);
         }
     }
 }
